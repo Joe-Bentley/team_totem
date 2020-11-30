6f0edc07a674b4e038b27463af51144c751832d2
Joe Bentley <joe.bentley.20@gmail.com>
2020-11-30 15:33:02 +0000

    Routing Setup
    #3 With relevant paths routing to desired URLs

---
+++ artifacts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path("", views.artifacts, name = "artifacts"),
+    path("artifacts/<int:artifact_id>/", views.artifact_info, name = "artifact_info"),
+    path("locations/", views.locations, name = "locations"),
+    path("locations/<int:location_id>/", views.location_info, name = "location_info"),
+    path("discovery/<int:year>/", views.discovered_year, name = "discovered_year"),
+    path("discovery/<int:year>/<int:month>/", views.discovered_year_month, name = "discovered_year_month"),
+    path("discovery/<int:year>/<int:month>/<int:day>/", views.discovered_year_month_day, name = "discovered_year_month_day"),
+]
