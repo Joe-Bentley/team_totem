190f51c28b1b6495cddf8833c7026c8beeee43bc
Joe Bentley <joe.bentley.20@gmail.com>
2020-11-30 14:56:52 +0000

    Updated Settings.py
    #1

--- totems/settings.py
+++ totems/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'artifacts.apps.ArtifactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
