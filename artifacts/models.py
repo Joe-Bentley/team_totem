eab05ce5fe74daa4520da9788764430af022dd29
Joe Bentley <joe.bentley.20@gmail.com>
2020-12-01 11:39:28 +0000

    Views.py testing
    Need to edit for templates

--- artifacts/models.py
+++ artifacts/models.py
@@ -4,13 +4,13 @@ from django.db import models
 from django.utils import timezone
 
 class Artifact(models.Model):
-    artifact_text = models.CharField(max_length=200)
-    artifact_info = models.TextField(blank = True)
+    text = models.CharField(max_length=200)
+    info = models.TextField(blank = True)
     discovered_by = models.CharField(max_length=200, default="unknown")
     pub_date = models.DateTimeField('Date Published', default=datetime.datetime.now)
 
     def __str__(self):
-        return self.artifact_text
+        return f'{self.text} : {self.info[:25]}...'
 
 class Location(models.Model):
     artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE)
