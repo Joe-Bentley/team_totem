5909305f99d7e1e5a54b2393469bc29a856f5a07
Joe Bentley <joe.bentley.20@gmail.com>
2020-12-01 10:32:50 +0000

    Created models
    #5 #6 Created Artifact + Location models

--- artifacts/models.py
+++ artifacts/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Artifact(models.Model):
+    artifact_text = models.CharField(max_length=200)
+    artifact_info = models.TextField(blank = True)
+    discovered_by = models.CharField(max_length=200, default="unknown")
+    pub_date = models.DateTimeField('Date Published', default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.artifact_text
+
+class Location(models.Model):
+    artifact = models.ForeignKey(Artifact, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
